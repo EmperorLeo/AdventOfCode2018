f002462c07d93a08233b3d235a402a71107d9bbc
Leo Feldman <laf22@duke.edu>
2018-12-02 22:25:46 -0500

    day1 for golang complete

--- go/day1.go
+++ go/day1.go
@@ -8,16 +8,34 @@ import (
 )
 
 func main() {
+	// Part 1
 	file, err := os.Open("../input/day1.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
+	var frequencies []int
 	var freq int
 	for scanner.Scan() {
 		val, _ := strconv.Atoi(scanner.Text())
 		freq += val
+		frequencies = append(frequencies, val)
 	}
 	fmt.Println(fmt.Sprintf("Frequency is %d", freq))
+
+	// Part 2
+	visitedSet := make(map[int]bool)
+	freq = 0
+	for {
+		for _, frequency := range frequencies {
+			freq += frequency
+			_, exists := visitedSet[freq]
+			visitedSet[freq] = true
+			if exists {
+				fmt.Println(fmt.Sprintf("Repeated frequency is %d", freq))
+				return
+			}
+		}
+	}
 }
