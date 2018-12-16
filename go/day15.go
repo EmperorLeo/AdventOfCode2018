81ecf92f5e29db05f14947627d8f0539ef4aeea0
Leo Feldman <laf22@duke.edu>
2018-12-15 19:00:31 -0500

    will day 15 work? almost 300 lines of code

--- go/day15.go
+++ go/day15.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 )
+
+type coordinate struct {
+	x, y int
+}
 
 type unit struct {
 	x, y, hp, ap int
@@ -18,11 +23,18 @@ type cavern struct {
 	turn    int
 }
 
+type coordinateDepth struct {
+	c     coordinate
+	depth int
+}
+
 func day16() {
 	game := cavern{}
 	game.init()
 	for !game.winnerFound() {
+		game.sortUnits()
 		game.incrementTurn()
+		game.cleanUpTheDead()
 	}
 	winner, totalHp := game.getWinner()
 	fmt.Printf("Team %q won with outcome (totalHp * rounds) = %d * %d = %d", winner, totalHp, game.turn, totalHp*game.turn)
@@ -59,8 +71,56 @@ func (c *cavern) init() {
 
 func (c *cavern) incrementTurn() {
 	c.turn++
-	for _, unit := range c.units {
-		// TODO
+	for _, u := range c.units {
+		if u.hp <= 0 {
+			// Need to deal with this if the unit has not been cleaned up yet
+			continue
+		}
+
+		var enemies []*unit
+		if u.badGuy {
+			enemies = c.elves
+		} else {
+			enemies = c.goblins
+		}
+		u.takeTurn(c.caveMap, enemies)
+	}
+}
+
+func (c *cavern) cleanUpTheDead() {
+	dead := []*unit{}
+	for _, u := range c.units {
+		if u.hp <= 0 {
+			dead = append(dead, u)
+		}
+	}
+
+	for _, deadUnit := range dead {
+		var unitIndex int
+		for i, u := range c.units {
+			if u == deadUnit {
+				unitIndex = i
+				break
+			}
+		}
+		c.units = remove(c.units, unitIndex)
+		if deadUnit.badGuy {
+			for i, u := range c.goblins {
+				if u == deadUnit {
+					unitIndex = i
+					break
+				}
+			}
+			c.goblins = remove(c.goblins, unitIndex)
+		} else {
+			for i, u := range c.elves {
+				if u == deadUnit {
+					unitIndex = i
+					break
+				}
+			}
+			c.elves = remove(c.elves, unitIndex)
+		}
 	}
 }
 
@@ -71,9 +131,9 @@ func (c *cavern) winnerFound() bool {
 func (c *cavern) getWinner() (rune, int) {
 	var winner rune
 	if len(c.elves) == 0 {
-		winner = 'E'
-	} else {
 		winner = 'G'
+	} else {
+		winner = 'E'
 	}
 
 	var totalHp int
@@ -94,4 +154,127 @@ func (c *cavern) sortUnits() {
 			return c.units[i].x < c.units[j].x
 		}
 	})
+}
+
+func (u *unit) takeTurn(graph [][]bool, enemies []*unit) {
+	var adjacentToTarget *unit
+	var destinationCoord coordinate
+	var currentNextCoord coordinate
+	currentPathLength := math.MaxInt32
+	for _, enemy := range enemies {
+		if u.isAdjacentTo(enemy) {
+			if adjacentToTarget == nil || adjacentToTarget != nil && compareAdjacent(enemy, adjacentToTarget) {
+				adjacentToTarget = enemy
+			}
+			currentPathLength = 0
+		} else if currentPathLength > 0 {
+			for i := -1; i <= 1; i += 2 {
+				for j := -1; j <= 1; j += 2 {
+					coord := coordinate{u.x + i, u.y + j}
+					nextCoord, pathLength := u.getShortestPath(coord, graph)
+					if pathLength != -1 {
+						if currentPathLength > pathLength || currentPathLength == pathLength && compareReadingDistance(coord, destinationCoord) {
+							destinationCoord = coord
+							currentPathLength = pathLength
+							currentNextCoord = nextCoord
+						}
+					}
+				}
+			}
+		}
+
+		// If an adjacent enemy or a path was found
+		if adjacentToTarget != nil {
+			u.attack(adjacentToTarget)
+		} else if currentPathLength != math.MaxInt32 {
+			// Open up the area once the unit leaves
+			graph[u.y][u.x] = true
+			u.x = currentNextCoord.x
+			u.y = currentNextCoord.y
+			// Close up the area once the unit enters
+			graph[u.y][u.x] = false
+		}
+	}
+}
+
+func (u *unit) isAdjacentTo(enemy *unit) bool {
+	return u.x == enemy.x && int(math.Abs(float64(u.y-enemy.y))) == 1 || u.y == enemy.y && int(math.Abs(float64(u.x-enemy.x))) == 1
+}
+
+func (u *unit) getShortestPath(c coordinate, graph [][]bool) (coordinate, int) {
+	// Implement BFS algorithm to find shortest path (modified Dijkstra's algorithm based on the distance always being 1)
+	initialCoordinate := coordinate{u.x, u.y}
+	visitedMap := map[coordinate]int{initialCoordinate: -1}
+	queue := []coordinateDepth{}
+	for len(queue) > 0 {
+		queueItem := queue[0]
+		queue = queue[1:]
+		newDepth := queueItem.depth + 1
+		if queueItem.depth == -1 {
+			// Take care of first item, which has a depth of -1
+			newDepth = 1
+		}
+
+		for i := -1; i <= 1; i += 2 {
+			for j := -1; j <= 1; j += 2 {
+				coord := coordinate{queueItem.c.x + i, queueItem.c.y + j}
+				if coord.canTraverse(graph) && visitedMap[coord] == 0 {
+					visitedMap[coord] = newDepth
+					queue = append(queue, coordinateDepth{coord, newDepth})
+				}
+				if coord == c {
+					return coord, newDepth
+				}
+			}
+		}
+	}
+
+	return coordinate{}, -1
+}
+
+func (u *unit) attack(enemy *unit) {
+	enemy.hp -= u.ap
+}
+
+func (c *coordinate) canTraverse(graph [][]bool) bool {
+	if c.y < 0 || c.y >= len(graph) {
+		return false
+	}
+
+	if c.x < 0 || c.x >= len(graph[c.y]) {
+		return false
+	}
+
+	return graph[c.y][c.x]
+}
+
+func compareAdjacent(a *unit, b *unit) bool {
+	if a.hp < b.hp {
+		return true
+	} else if a.hp > b.hp {
+		return false
+	} else {
+		if a.y < b.y {
+			return true
+		} else if a.y > b.y {
+			return false
+		} else {
+			return a.x < b.x
+		}
+	}
+}
+
+func compareReadingDistance(a coordinate, b coordinate) bool {
+	if a.y < b.y {
+		return true
+	} else if a.y > b.y {
+		return false
+	} else {
+		return a.x < b.x
+	}
+}
+
+func remove(units []*unit, index int) []*unit {
+	units[index] = units[len(units)-1]
+	return units[:len(units)-1]
 }
