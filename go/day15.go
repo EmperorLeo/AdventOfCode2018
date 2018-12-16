c29626a43bcc85b9d24e5c5a6fb346f5d07e9717
Leo Feldman <laf22@duke.edu>
2018-12-15 23:38:21 -0500

    so close :(

--- go/day15.go
+++ go/day15.go
@@ -6,6 +6,8 @@ import (
 	"sort"
 )
 
+var coordIncrementor = []coordinate{coordinate{0, -1}, coordinate{-1, 0}, coordinate{1, 0}, coordinate{0, 1}}
+
 type coordinate struct {
 	x, y int
 }
@@ -28,16 +30,21 @@ type coordinateDepth struct {
 	depth int
 }
 
-func day16() {
+func day15() {
 	game := cavern{}
 	game.init()
 	for !game.winnerFound() {
+		game.print()
+		fmt.Println()
 		game.sortUnits()
 		game.incrementTurn()
+		game.printScoreboard()
 		game.cleanUpTheDead()
+		// time.Sleep(2 * time.Second)
 	}
 	winner, totalHp := game.getWinner()
-	fmt.Printf("Team %q won with outcome (totalHp * rounds) = %d * %d = %d", winner, totalHp, game.turn, totalHp*game.turn)
+	fmt.Printf("Team %q won with outcome (totalHp * rounds) = %d * %d = %d", winner, totalHp, game.turn-1, totalHp*game.turn-1)
+	fmt.Println()
 }
 
 func (c *cavern) init() {
@@ -67,12 +74,22 @@ func (c *cavern) init() {
 			}
 		}
 	}
+	c.units = units
+	c.elves = elves
+	c.goblins = goblins
+	c.caveMap = caveMap
+	fmt.Printf("There are %d elves.\n", len(c.elves))
+	fmt.Printf("There are %d goblins.\n", len(c.goblins))
+	fmt.Printf("There are %d total units.\n", len(c.units))
 }
 
 func (c *cavern) incrementTurn() {
 	c.turn++
 	for _, u := range c.units {
+		// c.print()
 		if u.hp <= 0 {
+			fmt.Printf("Dead elf was not cleaned up")
+			fmt.Println()
 			// Need to deal with this if the unit has not been cleaned up yet
 			continue
 		}
@@ -84,6 +101,7 @@ func (c *cavern) incrementTurn() {
 			enemies = c.goblins
 		}
 		u.takeTurn(c.caveMap, enemies)
+		// time.Sleep(2 * time.Second)
 	}
 }
 
@@ -96,6 +114,7 @@ func (c *cavern) cleanUpTheDead() {
 	}
 
 	for _, deadUnit := range dead {
+		c.caveMap[deadUnit.y][deadUnit.x] = true
 		var unitIndex int
 		for i, u := range c.units {
 			if u == deadUnit {
@@ -156,43 +175,95 @@ func (c *cavern) sortUnits() {
 	})
 }
 
+func (c *cavern) print() {
+	for y, row := range c.caveMap {
+		stringToPrint := ""
+		for x, val := range row {
+			var curUnit *unit
+			for _, u := range c.units {
+				if u.x == x && u.y == y {
+					curUnit = u
+					break
+				}
+			}
+
+			if curUnit != nil {
+				if curUnit.badGuy {
+					stringToPrint += "G"
+				} else {
+					stringToPrint += "E"
+				}
+			} else if val {
+				stringToPrint += "."
+			} else {
+				stringToPrint += "#"
+			}
+		}
+		fmt.Print(stringToPrint)
+		fmt.Println()
+	}
+}
+
+func (c *cavern) printScoreboard() {
+	fmt.Printf("ROUNDNUMBER: %d", c.turn)
+	fmt.Println()
+	for _, u := range c.units {
+		var team string
+		if u.badGuy {
+			team = "Goblin"
+		} else {
+			team = "Elf"
+		}
+		fmt.Printf("%s with hp %d", team, u.hp)
+		fmt.Println()
+	}
+}
+
 func (u *unit) takeTurn(graph [][]bool, enemies []*unit) {
 	var adjacentToTarget *unit
+	var enemyToTravelTo *unit
 	var destinationCoord coordinate
 	var currentNextCoord coordinate
 	currentPathLength := math.MaxInt32
 	for _, enemy := range enemies {
+		if enemy.hp <= 0 {
+			// Enemy not cleaned up yet
+			continue
+		}
+
 		if u.isAdjacentTo(enemy) {
 			if adjacentToTarget == nil || adjacentToTarget != nil && compareAdjacent(enemy, adjacentToTarget) {
 				adjacentToTarget = enemy
 			}
 			currentPathLength = 0
 		} else if currentPathLength > 0 {
-			for i := -1; i <= 1; i += 2 {
-				for j := -1; j <= 1; j += 2 {
-					coord := coordinate{u.x + i, u.y + j}
-					nextCoord, pathLength := u.getShortestPath(coord, graph)
-					if pathLength != -1 {
-						if currentPathLength > pathLength || currentPathLength == pathLength && compareReadingDistance(coord, destinationCoord) {
-							destinationCoord = coord
-							currentPathLength = pathLength
-							currentNextCoord = nextCoord
-						}
+			for _, incrementor := range coordIncrementor {
+				coord := coordinate{enemy.x + incrementor.x, enemy.y + incrementor.y}
+				nextCoord, pathLength := u.getShortestPath(coord, graph)
+				if pathLength != -1 {
+					if currentPathLength > pathLength || currentPathLength == pathLength && compareReadingDistance(coord, destinationCoord) {
+						destinationCoord = coord
+						currentPathLength = pathLength
+						currentNextCoord = nextCoord
+						enemyToTravelTo = enemy
 					}
 				}
 			}
 		}
-
-		// If an adjacent enemy or a path was found
-		if adjacentToTarget != nil {
-			u.attack(adjacentToTarget)
-		} else if currentPathLength != math.MaxInt32 {
-			// Open up the area once the unit leaves
-			graph[u.y][u.x] = true
-			u.x = currentNextCoord.x
-			u.y = currentNextCoord.y
-			// Close up the area once the unit enters
-			graph[u.y][u.x] = false
+	}
+	// If an adjacent enemy or a path was found
+	if adjacentToTarget != nil {
+		u.attack(adjacentToTarget)
+	} else if currentPathLength != math.MaxInt32 {
+		// Open up the area once the unit leaves
+		graph[u.y][u.x] = true
+		u.x = currentNextCoord.x
+		u.y = currentNextCoord.y
+		// Close up the area once the unit enters
+		graph[u.y][u.x] = false
+		// Handle case where unit is now adjacent to enemy
+		if u.isAdjacentTo(enemyToTravelTo) {
+			u.attack(enemyToTravelTo)
 		}
 	}
 }
@@ -205,24 +276,66 @@ func (u *unit) getShortestPath(c coordinate, graph [][]bool) (coordinate, int) {
 	// Implement BFS algorithm to find shortest path (modified Dijkstra's algorithm based on the distance always being 1)
 	initialCoordinate := coordinate{u.x, u.y}
 	visitedMap := map[coordinate]int{initialCoordinate: -1}
+	backwardsMap := map[coordinate]*coordinate{}
 	queue := []coordinateDepth{}
+	top := coordinate{u.x, u.y - 1}
+	left := coordinate{u.x - 1, u.y}
+	right := coordinate{u.x + 1, u.y}
+	bottom := coordinate{u.x, u.y + 1}
+	// Kinda hacky so the backwards map ends up at these nodes
+	if top.canTraverse(graph) {
+		queue = append(queue, coordinateDepth{top, 1})
+		visitedMap[top] = 1
+		if top == c {
+			return top, 1
+		}
+	}
+	if left.canTraverse(graph) {
+		queue = append(queue, coordinateDepth{left, 1})
+		visitedMap[left] = 1
+		if left == c {
+			return left, 1
+		}
+	}
+	if right.canTraverse(graph) {
+		queue = append(queue, coordinateDepth{right, 1})
+		visitedMap[right] = 1
+		if right == c {
+			return right, 1
+		}
+	}
+	if bottom.canTraverse(graph) {
+		queue = append(queue, coordinateDepth{bottom, 1})
+		visitedMap[bottom] = 1
+		if bottom == c {
+			return bottom, 1
+		}
+	}
+
 	for len(queue) > 0 {
 		queueItem := queue[0]
+		// fmt.Printf("Testing coordinate")
 		queue = queue[1:]
 		newDepth := queueItem.depth + 1
-		if queueItem.depth == -1 {
-			// Take care of first item, which has a depth of -1
-			newDepth = 1
-		}
+		for _, incrementor := range coordIncrementor {
+			coord := coordinate{queueItem.c.x + incrementor.x, queueItem.c.y + incrementor.y}
+			// fmt.Print(coord.canTraverse(graph))
+			// fmt.Println()
+			if coord.canTraverse(graph) && visitedMap[coord] == 0 {
+				visitedMap[coord] = newDepth
+				queue = append(queue, coordinateDepth{coord, newDepth})
+				backwardsMap[coord] = &queueItem.c
 
-		for i := -1; i <= 1; i += 2 {
-			for j := -1; j <= 1; j += 2 {
-				coord := coordinate{queueItem.c.x + i, queueItem.c.y + j}
-				if coord.canTraverse(graph) && visitedMap[coord] == 0 {
-					visitedMap[coord] = newDepth
-					queue = append(queue, coordinateDepth{coord, newDepth})
-				}
 				if coord == c {
+					// fmt.Printf("Unit wants to go to %d, %d", coord.x, coord.y)
+					// fmt.Println()
+					for backwardsMap[coord] != nil {
+						// Traverse backwards until you get to the first step
+						coord = *backwardsMap[coord]
+						// time.Sleep(time.Second)
+						// fmt.Printf("Backwards path: %d, %d", coord.x, coord.y)
+						// fmt.Println()
+					}
 					return coord, newDepth
 				}
 			}
--- go/program.go
+++ go/program.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	day14()
+	day15()
 }
