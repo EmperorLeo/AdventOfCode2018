99f548366a553cd870053700b64376a18c8bd773
Leo Feldman <laf22@duke.edu>
2019-01-04 13:22:30 -0500

    more day 24

--- go/day24.go
+++ go/day24.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"regexp"
+)
+
 type battleGroup struct {
 	team, totalHp, hpPerUnit, ad, initiative int
 	adType                                   rune
@@ -12,5 +16,41 @@ type immuneSystemSimulator struct {
 }
 
 func day24() {
+}
 
+func (bg *battleGroup) init() {
+	regex := regexp.MustCompile(`^(\d+) units each with (\d+) hit points (?:\((.*)\) )?with an attack that does (\d+) (fire|cold|slashing) damage at initiative (\d+)$`)
+	lines := readFile("../input/day24.txt")
+	groups := []*battleGroup{}
+	immuneSystem := []*battleGroup{}
+	infection := []*battleGroup{}
+	var team int
+	for _, line := range lines {
+		matches := regex.FindAllStringSubmatch("", -1)
+		if matches == nil {
+			if line == "Immune System:" {
+				team = 0
+			} else if line == "Infection:" {
+				team = 1
+			}
+		} else {
+			bg := battleGroupFromMatch(matches[0])
+			groups = append(groups, bg)
+			if team == 0 {
+				immuneSystem = append(immuneSystem, bg)
+			} else {
+				infection = append(infection, bg)
+			}
+		}
+	}
+}
+
+func battleGroupFromMatch(match []string) *battleGroup {
+	numUnits := match[1]
+	hp := match[2]
+	buffInfo := match[3]
+	ad := match[4]
+	adType := match[5]
+
+	return nil
 }
